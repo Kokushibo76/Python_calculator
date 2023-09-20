cf62ac57ac806033e628585c2446761bc05fc6d5
Kokushibo76 <nfrolov@gmail.com>
2023-09-20 14:43:29 +0300

    first commit

---
+++ Calculator PYTHON.py
@@ -0,0 +1,51 @@
+import math
+print('Выбирете действие:')
+print('1)Сложение  6)Корень числа')
+print('2)Вычитание 7)Факториал числа')
+print('3)Умножение 8)Синус числа')
+print('4)Деление   9)Косинус числа')
+print('5)Степень   10)Тангенс числа')
+func = int(input('Ваш выбор: '))
+if func >= 11:
+    print('Вы выбрали неправельное действие')
+else:
+    def go(func):
+        match func:
+            case 1:
+                x = float(input('Введите первое число: '))
+                y = float(input('Введите второе число: '))
+                print('Результат: ',x+y)
+            case 2:
+                x = float(input('Введите первое число: '))
+                y = float(input('Введите второе число: '))
+                print('Результат: ',x-y)
+            case 3:
+                x = float(input('Введите первое число: '))
+                y = float(input('Введите второе число: '))
+                print('Результат: ',x*y)
+            case 4:
+                x = float(input('Введите делимое число: '))
+                y = float(input('Введите делитель число: '))
+                if y == 0:
+                    print('НЕПРАВИЛЬНО! Делить на 0 запрещено')
+                print ('Результат: ',x/y)
+            case 5:
+                x = float(input('Введите число: '))
+                y = float(input('Введите степень: '))
+                print ('Результат: ',x**y)
+            case 6:
+                x = float(input('Введите число: '))
+                print ('Результат: ',(math.sqrt(x)))
+            case 7:
+                x = int(input('Введите число: '))
+                print ('Результат: ', (math.factorial(x)))
+            case 8:
+                x = float(input('Введите число: '))
+                print ('Результат: ',(math.sin(x)))
+            case 9:
+                x = float(input('Введите число: '))
+                print ('Результат: ',(math.cos(x)))
+            case 10:
+                x = float(input('Введите число: '))
+                print ('Результат: ',(math.tan(x)))
+go(func)
